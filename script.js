213ec1291ddcece6ddbd861625072e5cc0e78bd7
firouzeh <firouzeh.afkhami@gmail.com>
2024-09-07 11:41:04 -0400

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function toggleMenu() {
-  const sidenav = document.getElementById('sidenav');
-  const isOpen = sidenav.classList.contains('open');
-  if (isOpen) {
-      sidenav.classList.remove('open');
-  } else {
-      sidenav.classList.add('open');
-  }
-}
+// Select the elements
+// Select the elements
+var menuBtn = document.getElementById("menuBtn");
+var sidenav = document.getElementById("sidenav");
 
-function closeMenu() {
-  document.getElementById('sidenav').classList.remove('open');
+// Set the initial position of sidenav
+sidenav.style.right = "-250px";
+
+// Toggle sidenav position on menu button click
+menuBtn.onclick = function() {
+    // Check if sidenav is off-screen
+    if (sidenav.style.right === "-250px" || sidenav.style.right === "") {
+        sidenav.style.right = "0"; // Show sidenav
+        menu.src = "images/close.png";
+    } else {
+        sidenav.style.right = "-250px"; // Hide sidenav
+        menu.src = "images/menu.png";
+    }
 }
